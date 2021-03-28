98dafc7b6d8f2cdcda68790c73e646b10d083812
Nick Manton <nick@mantonbradbury.com>
2021-03-28 11:49:37 +0100

    update package/env variable

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 // jshint node: true, esversion: 6
 'use strict';
 
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const state = { pool: null, mode: null };
 const debug = true;
 
 exports.conn = done => {
   // environment variable to pass connection parameters, including utf8mb4 charset (for emoji)
-  state.pool = mysql.createPool(process.env.BACALL_DB);
+  state.pool = mysql.createPool(process.env.OSCAR_DB);
   done();
 }
 
