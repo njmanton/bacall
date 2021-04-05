9345693b42720249f3795d68cf5e49cf219163ad
Nick Manton <nick@mantonbradbury.com>
2021-04-05 11:41:58 +0100

    rewrite function to fetch images from tmdb

--- models/tmdb.js
+++ models/tmdb.js
@@ -1,50 +1,64 @@
 // jshint node: true, esversion: 6
 'use strict';
 
-const key = process.env.TMDB_API;
-const mdb = require('moviedb')(key);
+const key = process.env.TMDB_API,
+      mdb = require('moviedb')(key),
+      db  = require('../models/');
 
 const tmdb = {
 
-  // get a path to a random image from tmdb
-  // if a movie (item = 0), get a random poster and backdrop
-  // if a person (item = 1), just return image
-  images: (item, done) => {
-    if (!item.id) {
-      done({ error: 'no id provided' });
+  // get random images, incl. backdrop for movie types for the winner
+  images: (category, done) => {
+    if (!category || category < 1 || category > 25) {
+      done({ error: 'invalid category' });
     } else {
-      if (item.type == 0) {
-        mdb.movieImages({ id: item.id }, (err, res) => {
+      const sql = 'SELECT C.class, C.name AS cname, C.winner_id, N.id, N.name AS wname, N.tmdb_id, COUNT(P.id) AS preds, SUM(P.nominee_id = C.winner_id) AS correct FROM categories C LEFT JOIN nominees N ON C.winner_id = N.id LEFT JOIN predictions P ON P.category_id = C.id WHERE C.id = ?;'
+      db.use().query(sql, category, (err, rows) => {
+        const winner = rows[0] || []; // get the winner data
+        let data = {
+          error:null, 
+          poster: null, 
+          backdrop: null,
+          category: winner.cname,
+          winner: winner.wname, 
+          correct: winner.correct, 
+          points: Math.round((winner.preds / winner.correct) * 100) / 100,
+          singular: (winner.correct == 1)
+        };
 
-          try {
-            if (!res) throw new Error (`can't find that resource`);
-            const idx = Math.floor(Math.random() * (res.backdrops.length - 1)),
-                  idx2 = Math.floor(Math.random() * (res.posters.length - 1));            
-            done({
-              poster: res.posters[idx2].file_path,
-              backdrop: res.backdrops[idx].file_path,
-            });            
-          } catch(e) {
-            console.log(e);
-            done({ error: e.message });
-          }
+        if (winner.class == 0) { // nominee is a movie
+          mdb.movieImages({ id: winner.tmdb_id }, (err, res) => {
+            try {
+              if (!res) throw new Error (`can't find that resource`);
+              const idx = Math.floor(Math.random() * (res.backdrops.length - 1)),
+                    idx2 = Math.floor(Math.random() * (res.posters.length - 1));
+              data.poster = res.posters[idx2].file_path;
+              data.backdrop = res.backdrops[idx].file_path;
+              done(data);
+            } catch(e) {
+              data.error = e.message;
+              done(data);
+            }
 
-        })
-      } else {
-        mdb.personImages({ id: item.id }, (err, res) => {
-          try {
-            const idx = Math.floor(Math.random() * (res.profiles.length - 1));
-            done({
-              poster: res.profiles[idx].file_path,
-              backdrop: null
-            });
-          } catch(e) {
-            done({ error: true });
-          }
-        })
-      }
+          });
+        } else if (winner.class == 1) { // nominee is a person 
+          mdb.personImages({ id: winner.tmdb_id }, (err, res) => {
+            try {
+              if (!res) throw new Error(`can't find that resource`);
+              const idx = Math.floor(Math.random() * (res.profiles.length - 1));
+              data.poster = res.profiles[idx].file_path;
+              done(data);
+            } catch(e) {
+              data.error = e.message;
+              done(data);
+            }
+          });
+        } else {
+          data.error = 'no class';
+          done(data);
+        }
+      });
     }
-
   }
 
 }
