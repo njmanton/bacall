615316f4dee3ceab20d00c6978a6c9c269f17781
Nick Manton <nick@mantonbradbury.com>
2022-03-24 21:26:34 +0000

    add new placeholders
    add some new placeholders and move them to separate json file

--- config/index.js
+++ config/index.js
@@ -7,19 +7,8 @@ exports.exp_test = exp_test;
 const deadline_date = '2022-03-27T23:00Z';
 exports.deadline = new Date(deadline_date);;
 
-const signups = [
-  { uid: 'rubeus', email: 'rhagrid@hogwarts.ac.uk' },
-  { uid: 'dan', email: 'd.truman@nasa.gov' },
-  { uid: 'holly', email: 'gennaro@nakatomi.com' },
-  { uid: 'mbd', email: 'milesdyson@cyberdyne.com' },
-  { uid: 'stan', email: 'goodspeed@fbi.gov' },
-  { uid: 'ellen', email: 'ripley@wayland-yutani.com' },
-  { uid: 'eldon', email: 'eldon@tyrell.com' },
-  { uid: 'tyler', email: 'durden@projectmayhem.org' },
-  { uid: 'norville', email: 'nbarnes@hudsucker.com' },
-  { uid: 'fatamy', email: 'phobart@barden.edu' }
-];
-
 exports.placeholders = () => {
-  return signups[Math.floor(Math.random() * signups.length)];
+  const signups = require('./signups.json'),
+        rnd = Math.floor(Math.random() * signups.length);
+  return signups[rnd];
 }
