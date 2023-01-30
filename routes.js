961d7384a00693d8e55b35ed1402da04dbc3363b
Nick Manton <nick@mantonbradbury.com>
2023-01-30 15:33:24 +0000

    add error handling and route to /live

--- routes.js
+++ routes.js
@@ -13,7 +13,13 @@ const routes = app => {
   // main page
   app.get('/', (req, res) => {
     const expired = (new Date() > config.deadline || config.exp_test);
-    res.render('main', { expired: expired, signups: config.placeholders() });
+    res.render('main', { expired: expired, signups: config.placeholders() }, (err, html) => {
+      if(err) {
+        res.status(500).send(`An interal error (${ err }) has occurred`);
+      } else {
+        res.send(html);
+      }
+    });      
   });
 
   // other static routes
@@ -25,25 +31,28 @@ const routes = app => {
     if (fs.existsSync(`./views/recap/analysis_${ req.params.code }.hbs`)) {
       res.render(`recap/analysis_${ req.params.code }`, { title: 'Recap' });
     } else {
-      res.status(404).render('404');
+      res.status(404).render('404', {});
     }
   });
 
   // get the summary table for a player - should only be available after deadline
   app.get('/summary/:id', (req, res) => {
     const expired = (new Date() > config.deadline || config.exp_test);
-    console.log(expired);
     if (!expired) {
       res.render('main', { expired: expired });
     } else {
       pred.summary(req.params.id, false, data => {
-        //res.send(`<pre>${ JSON.stringify(data, null, 2) }</pre>`);
-        res.render('summary', {
-          data: data.table,
-          total: data.total,
-          username: data.username,
-          title: `Summary for ${ data.username }`
-        });
+        if (!data.err) {
+          res.render('summary', {
+            data: data.table,
+            total: data.total,
+            username: data.username,
+            title: `Summary for ${ data.username }`
+          });          
+        } else {
+          res.status(500).send(`Sorry, that operation couldn't be completed ( ${ data.err }).`);
+        }
+
       })    
     }
   });
@@ -73,15 +82,19 @@ const routes = app => {
     // if passed as a query string, exclude bot users from scoreboard
     const nobots = (req.query.nobots && req.query.nobots == 'true');
     pred.results(nobots, data => {
-      res.render('scoreboard', { table: data, expired: true , title: 'Leaderboard' });
+      if (data.err) {
+        res.status(500).send(`Sorry, there was an error retrieving that data (${ data.err })`);
+      } else {
+        res.render('scoreboard', { table: data, expired: true , title: 'Leaderboard' });
+      }
     })
   });
 
   // show a list of results
   app.get('/results', (req, res) => {
     pred.list(data => {
-      if (data.error) {
-        res.status(500).send('sorry, there was an error retrieving the results');
+      if (data.err) {
+        res.status(500).send(`Sorry, there was an error retrieving the results (${ data.err })`);
       } else {
         res.render('results', { list: data, title: 'Results by Category' });
       }
@@ -116,7 +129,7 @@ const routes = app => {
         if (user.id) {
           // player exists, so retrieve predictions
           pred.preds(user.id, req.params.cat, data => {
-            if (data.code) { // returns a code property if there was an SQL error
+            if (data.err) { // returns a code property if there was an SQL error
               res.render('main', { 
                 message: true,
                 message_err: true,
@@ -125,25 +138,27 @@ const routes = app => {
             } else {
               // get the list of all categories for the navigation
               pred.list(cats => {
-                // loop through nominees to get prediction for the default image
-                let img = '';
-                for (let x = 0; x < data.length; x++) {
-                  if (data[x].pred) {
-                    // short-circuit the loop once we find a prediction
-                    img = data[x].image; break;
+                if (cats.err) {
+                  res.status(500).send(`Sorry, there was an error retrieving data (${ cats.err })`)
+                } else {
+                  // loop through nominees to get prediction for the default image
+                  let img = '';
+                  for (let x = 0; x < data.length; x++) {
+                    if (data[x].pred) {
+                      // short-circuit the loop once we find a prediction
+                      img = data[x].image; break;
+                    }
                   }
+                  res.render('players', {
+                    expired: expired,
+                    user: user,
+                    data: data,
+                    cat: cats[req.params.cat - 1],
+                    img: img,
+                    title: `Best ${ cats[req.params.cat - 1].name }`
+                  });                  
                 }
-                res.render('players', {
-                  expired: expired,
-                  user: user,
-                  data: data,
-                  cat: cats[req.params.cat - 1],
-                  img: img,
-                  title: `Best ${ cats[req.params.cat - 1].name }`
-                });
-                //res.send(`<pre>${ JSON.stringify([data, cats[req.params.cat - 1], img], null, 2) }</pre>`);
               })
-
             }
           })
         } else {
@@ -170,19 +185,23 @@ const routes = app => {
   // render a summary of given category
   app.get('/category/:cat', (req, res) => {
     tmdb.images(req.params.cat, data => {
-      res.render('category', {
-        layout: 'layout_cat',
-        data: data,
-        cat: req.params.cat,
-        title: `Best ${ data.category }`
-      });
-      //res.send(`<pre>${ JSON.stringify(data, null, 2) }</pre>`)
+      if (!data) {
+        res.status(500).send('Sorry, there was an error retrieving information for that category');
+      } else if (data.err == 'invalid category') {
+        res.render('main', { message: true, message_err: true, message_text: 'Category id must be between 1 and 23'});
+      } else {
+        res.render('category', {
+          layout: 'layout_cat',
+          data: data,
+          cat: req.params.cat,
+          title: `Best ${ data.category }`
+        });        
+      }
     })
   });
 
   // ajax request for pie chart on category page
   app.get('/api/cat/:cat', (req, res) => {
-    //res.send(['a', 'b', 'c']);
     pred.pie(req.params.cat, data => {
       res.send(data);
     })
@@ -202,9 +221,13 @@ const routes = app => {
     })
   })
 
+  app.get('/live', (req, res) => {
+    res.render('live', { title: 'demo chart' });
+  })
+
   // capture any other non-matching routes here
-  app.get('*', function(req, res){
-    res.status(404).render('404', { layout: 'layout_err', title: "Page not found" });
+  app.get('*', (req, res) => {
+    res.status(404).render('404', { layout: 'layout_err', title: "Page not found", ref: req.path });
   });
 }
 
