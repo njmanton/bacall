60fdae7a209a8912a676575d980ab2d7b8c18528
Nick Manton <nick@mantonbradbury.com>
2023-01-31 11:03:17 +0000

    refactor functions to use async/await

--- models/player.js
+++ models/player.js
@@ -7,42 +7,40 @@ const db = require('../models/'),
 
 const player = {
 
-  exists: (code, done) => {
+  exists: async (code, done) => {
 
-    if (!code || code.length != 8) {
+    if (!code || code.length !=8 ) {
       done({ err: 'Invalid code' });
     } else {
       const sql = 'SELECT id, username, franchise FROM users WHERE code = ? LIMIT 1';
       let result = { err: null, id: null, name: null }; // object to return to valid code check
-      db.use().promise().execute(sql, [code]).then(([rows, fields]) => {
-        if (!rows.length) {
-          result.err = 'Invalid code';
-        } else {
-          result = rows[0];
-        }
-      }).catch(err => {
-        result.err = err.code;
-      }).finally(() => {
-        done(result);
-      });
+      const qry = await db.use().promise().execute(sql, [code]);
+      const rows = qry[0];
+      if (!rows.length) {
+        result.err = 'Invalid code';
+      } else {
+        result = rows[0];
+      }
+      done(result);
     }
-
   },
 
   unique: (type, val, done) => {
 
-    const fld = (type == 1) ? 'username' : 'email';
     if (type == 1) {
       var sql = 'SELECT id FROM users WHERE username = ?';
     } else if (type == 2) {
       var sql = 'SELECT id FROM users WHERE email = ?';
     } else {
-      return false;
+      return null;
     }
     
     db.use().promise().execute(sql, [val]).then(([rows, fields]) => {
       done(rows.length == 0);
-    }).catch(err => { })
+    }).catch(err => {
+      logger.error(`Error in player.exists (${ err.code })`);
+      done(null);
+    })
 
   },
 
@@ -66,20 +64,27 @@ const player = {
       code += letters[idx];
     }
 
-    const sql = 'INSERT INTO users SEhT username = ?, email = ?, franchise = ?, code = ?, registered = ?';
+    const user = {
+      username: username, 
+      email: email, 
+      franchise: franchise,
+      code: code,
+      registered: new Date()
+    }
+
+    const sql = 'INSERT INTO users SET username = ?, email = ?, franchise = ?, code = ?, registered = ?';
     const result = {
       error: null,
       code: null
     };
     db.use().promise().execute(sql, [username, email, franchise, code, new Date()]).then(([rows, fields]) => {
       result.code = code;
-      // mail.send(email, user, result => {
-      //   logger.info(`signup email sent to ${ user.email }`);
-      // })
+      mail.send(email, user, result => {
+        logger.info(`signup email sent to ${ user.email }`);
+      })
     }).catch(err => { 
       logger.error(`Error processing signup for ${ email }. The code was '${ err.code }'`);
       result.error = true;
-      console.log(err);
     }).finally(() => {
       done(result);
     })
