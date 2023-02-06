0fb13203b1de822011b41814b8032457ca7d044e
Nick Manton <nick@mantonbradbury.com>
2023-02-06 16:25:01 +0000

    update tmdb image code
    kludge to allow the film poster to be returned in case of joint winners

--- models/tmdb.js
+++ models/tmdb.js
@@ -13,21 +13,23 @@ const tmdb = {
     if (!category || category < 1 || category > 25) {
       done({ err: 'invalid category' });
     } else {
-      const sql = 'SELECT C.class, C.name AS cname, C.winner_id, N.id, N.name AS wname, N.tmdb_id, MIN(P.score) AS score, COUNT(P.id) AS preds, SUM(P.nominee_id = C.winner_id) AS correct FROM categories C LEFT JOIN nominees N ON C.winner_id = N.id LEFT JOIN predictions P ON P.category_id = C.id WHERE C.id = ? GROUP BY C.class, C.name, C.winner_id, N.id, N.tmdb_id;'
+      const sql = 'SELECT C.class, C.name AS cname, C.winner_id, N.id, N.name AS wname, N.film AS film, N.tmdb_id, MIN(P.score) AS score, COUNT(P.id) AS preds, SUM(P.nominee_id = C.winner_id) AS correct FROM categories C LEFT JOIN nominees N ON C.winner_id = N.id LEFT JOIN predictions P ON P.category_id = C.id WHERE C.id = ? GROUP BY C.class, C.name, C.winner_id, N.id, N.tmdb_id;'
       db.use().promise().execute(sql, [category]).then(([rows,fields]) => {
         const winner = rows[0] || []; // get the winner data
+        console.log(winner);
         let data = {
           error:null, 
           poster: null, 
           backdrop: null,
           category: winner.cname,
           winner: winner.wname, 
+          film: winner.film,
           correct: winner.correct * 1, 
           points: (winner.score * 1).toFixed(2),
           singular: (winner.correct == 1)
         };
 
-        if (winner.class == 0) { // nominee is a movie
+        if (winner.class == 0 || winner.winner_id == 287) { // nominee is a movie (or multiple winners for EEAAO)
           mdb.movieImages({ id: winner.tmdb_id }, (err, res) => {
             try {
               if (!res) throw new Error (`can't find that resource`);
