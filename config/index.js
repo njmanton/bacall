3ad8ac0a4d176f59e400169121a843e9ba2e6fba
Nick Manton <nick@mantonbradbury.com>
2024-01-20 10:02:29 +0000

    update 2024 deadline

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@
 const exp_test = (process.argv[2] === 'expired') || false;
 exports.exp_test = exp_test;
 
-const deadline_date = '2023-03-12T23:00Z';
+const deadline_date = '2024-03-10T23:00Z';
 exports.deadline = new Date(deadline_date);;
 
 exports.placeholders = () => {
